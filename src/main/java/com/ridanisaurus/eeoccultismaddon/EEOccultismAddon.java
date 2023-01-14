3b0b51a53d17a66df4fb24f4ebddcc0036c61c00
Ridanis <khaled.chikhou@gmail.com>
2023-01-14 21:45:12 +0100

    1.0.0 Initial setup for EE Occultism Addon

--- src/main/java/com/ridanisaurus/eeoccultismaddon/EEOccultismAddon.java
+++ src/main/java/com/ridanisaurus/eeoccultismaddon/EEOccultismAddon.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(EEOccultismAddon.MOD_ID)
 public class EEOccultismAddon
 {
-    public static final String MOD_ID = "eeoccultismaddon";
+    public static final String MOD_ID = "ee_occultism_addon";
     public static final String OCCULTISM = "occultism";
 
     public EEOccultismAddon()
